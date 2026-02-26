fdf3fb7b85f567511189539d186ea97701bbdba5
michelvw <michel@vanwesten.org>
2026-02-26 09:02:40 +0100

    use supabase-js module from CDN

--- docs/supabase.js
+++ docs/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '../node_modules/@supabase/supabase-js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const supabase = createClient(
   'https://obiknpwjgiuxqskneuqx.supabase.co',
