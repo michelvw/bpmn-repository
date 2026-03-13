5a615c68f1be2e25e5fad4860a6b5b026c3b0bde
michelvw <michel@vanwesten.org>
2026-03-13 21:28:43 +0100

    btnTagsDropdown fix

--- docs/js/app.js
+++ docs/js/app.js
@@ -524,8 +524,8 @@ document.getElementById('btnSaveProfile').onclick = async () => {
 window.addEventListener('DOMContentLoaded', withErrorHandling(async () => {
   const hash = new URLSearchParams(window.location.hash.replace('#', '?'));
 
-  document.getElementById('btnTagsDropdown').closest('.dropdown').addEventListener('hide.bs.dropdown', (e) => {
-  if (e.clickEvent?.target?.closest('.color-option')) {
+  document.getElementById('btnTagsDropdown').closest('.btn-group').addEventListener('hide.bs.dropdown', (e) => {
+    if (e.clickEvent?.target?.closest('.color-option')) {
       e.preventDefault();
     }
   });
